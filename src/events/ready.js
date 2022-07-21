7e6f74c32d7341fc0b2ff9183f5d458b9a2df7d5
memte <63320170+memte@users.noreply.github.com>
2022-07-22 00:59:05 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,5 +4,5 @@ module.exports = {
 	once: true,
 	execute(client) {
     let activities = [ `Developed by memte#7601`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listenin }), 22000);
 }};
