e8a92557b5fb342a86c3b7f4a886ffb0c1d922c7
memte <63320170+memte@users.noreply.github.com>
2022-07-22 00:57:01 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,8 @@
+const { ActivityType } = require("discord.js")const 
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
     let activities = [ `Developed by memte#7601`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: "LISTENING" }), 22000);
+    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
 }};
