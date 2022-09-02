70faccf69fb3affab3437182e0bb750580191cf0
Mehmet <63320170+memte@users.noreply.github.com>
2022-09-02 20:46:41 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,19 +1,18 @@
 import { EmbedBuilder, InteractionType } from "discord.js";
 import { readdirSync } from "fs";
 import config from "../../config.js";
-const commandFiles = readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
  export default {
 	name: 'interactionCreate',
 	execute: async(interaction) => {
-  let client = interaction.client;
+   let client = interaction.client;
    if (!interaction.type == InteractionType.ApplicationCommand) return;
    if(interaction.user.bot) return;
 
-	for (const file of commandFiles) {
+   readdirSync('./src/commands').forEach(async file => {
         const command = await import(`../../src/commands/${file}`).then(x => x.default)
         if(interaction.commandName.toLowerCase() === command.data.name.toLowerCase()) {
         command.run(client, interaction)
     }
-	}
+	})
   }}
