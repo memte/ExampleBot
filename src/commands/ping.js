8dd180a7d61e7ce5b3d9397c4d8ed3bae05ae366
Mehmet <63320170+memte@users.noreply.github.com>
2022-12-04 18:53:36 +0300

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,8 +5,8 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pong!"),
+    // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
       interaction.reply(`Pong 🏓`)
-      // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
     }
  };
