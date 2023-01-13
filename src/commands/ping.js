814dd4441340f9384bd28619b37152269485c88e
Mehmet <63320170+memte@users.noreply.github.com>
2023-01-13 17:46:56 +0300

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pong!"),
-    // komuta seçenekler eklemek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
+    // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
     run: async (client, interaction) => {
       interaction.reply(`Pong 🏓`)
     }
