b3c6515840f30731191f7abbf0a5cccc3bcdc569
InFlames <85372327+InFlames513@users.noreply.github.com>
2023-01-17 00:24:26 +0300

    Düzeltmeler
    Gereksiz işlemler silindi ve kod daha optimizasyonlu bir şekilde tekrar yazıldı.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,18 +1,16 @@
-const { EmbedBuilder, InteractionType } = require("discord.js");
-const { readdirSync } = require("fs");
-
- module.exports = {
-	name: 'interactionCreate',
-	execute: async(interaction) => {
-  let client = interaction.client;
-   if (interaction.type == InteractionType.ApplicationCommand) {
-   if(interaction.user.bot) return;
-
-	readdirSync('./src/commands').forEach(file => {
-        const command = require(`../../src/commands/${file}`);
-        if(interaction.commandName.toLowerCase() === command.data.name.toLowerCase()) {
-        command.run(client, interaction)
-    }
-	})
-}
-  }}
+const { InteractionType } = require("discord.js");
+module.exports = {
+  name: 'interactionCreate',
+  execute: async(interaction) => {
+  try {
+      let client = interaction.client;
+      if (interaction.type == InteractionType.ApplicationCommand) {
+        const command = client.commands.get(interaction.commandName)
+        if(!command) return;
+        command.run(client, interaction);
+      }
+  } catch(e) {
+     interaction.reply({ content: "Komut çalıştırılamadı :/", ephemeral: true });
+     console.log(e);
+  }
+}}
