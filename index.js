fe2225efd85c8a5ab518023fe1dc6ed9da2e1d6f
InFlames <85372327+InFlames513@users.noreply.github.com>
2023-01-16 23:57:05 +0300

    Ufak iyileştirmeler.
    İntentlerin hepsini daha kısa bir koda açtık ve gereksiz parantezleri kaldırdık.

--- index.js
+++ index.js
@@ -1,59 +1,46 @@
-const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const { Client, GatewayIntentBits, Events, Partials } = require("discord.js");
+const client = new Client({ intents: Object.values(GatewayIntentBits).filter(x => typeof x === "string"), partials: Object.values(Partials).filter(x => typeof x === "string")});const config = require("./src/config.js");
 const config = require("./src/config.js");
-const { readdirSync } = require("fs")
+const { readdirSync } = require("node:fs")
 const moment = require("moment");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 
-let token = config.token
-
 client.commands = new Collection()
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(config.token);
 
 const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
 
 //command-handler
 const commands = [];
-readdirSync('./src/commands').forEach(async file => {
+readdirSync('./src/commands').forEach(file => {
   const command = require(`./src/commands/${file}`);
   commands.push(command.data.toJSON());
   client.commands.set(command.data.name, command);
 })
 
-client.on("ready", async () => {
+client.once("ready", async () => {
         try {
             await rest.put(
                 Routes.applicationCommands(client.user.id),
                 { body: commands },
             );
-        } catch (error) {
-            console.error(error);
-        }
+        } catch (error) console.error(error);
     log(`${client.user.username} Aktif Edildi!`);
 })
 
 //event-handler
 readdirSync('./src/events').forEach(async file => {
 	const event = require(`./src/events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+	if (event.once) client.once(event.name, (...args) => event.execute(...args));
+	else client.on(event.name, (...args) => event.execute(...args));
 })
 
 //nodejs-events
-process.on("unhandledRejection", e => { 
-   console.log(e)
- }) 
-process.on("uncaughtException", e => { 
-   console.log(e)
- })  
-process.on("uncaughtExceptionMonitor", e => { 
-   console.log(e)
- })
+process.on("unhandledRejection", e => console.log(e))
+process.on("uncaughtException", e => console.log(e))
+process.on("uncaughtExceptionMonitor", e => console.log(e))
 //
 
-client.login(token)
+client.login(config.token)
