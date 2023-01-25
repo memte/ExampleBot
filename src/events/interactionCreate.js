97e8e6ffe9eac4675c3f0bcec9dbd411e3bcb621
Mehmet <63320170+memte@users.noreply.github.com>
2023-01-25 19:19:46 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,12 +1,16 @@
 import { InteractionType } from "discord.js";
 
- export default {
+export default {
 	name: 'interactionCreate',
 	execute: async(interaction) => {
          let client = interaction.client;
    	 if (interaction.type == InteractionType.ApplicationCommand) {
    	 if(interaction.user.bot) return;
+	try {
          const command = client.commands.get(interaction.commandName)
          command.run(client, interaction)
+	} catch {
+	interaction.reply({content: "Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.})
+	}
     }
   }}
