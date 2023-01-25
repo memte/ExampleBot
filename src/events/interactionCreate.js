fc84e3f5a8be5b2bbf90218d48d9e2e5d6a036b4
Mehmet <63320170+memte@users.noreply.github.com>
2023-01-25 19:11:28 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,10 +3,10 @@ const { InteractionType } = require("discord.js");
  module.exports = {
 	name: 'interactionCreate',
 	execute: async(interaction) => {
-  let client = interaction.client;
-   	if (interaction.type == InteractionType.ApplicationCommand) {
-   	if(interaction.user.bot) return;
-        const command = client.commands.get(interaction.commandName)
-        command.run(client, interaction)
+         let client = interaction.client;
+   	 if (interaction.type == InteractionType.ApplicationCommand) {
+   	 if(interaction.user.bot) return;
+         const command = client.commands.get(interaction.commandName)
+         command.run(client, interaction)
     }
   }}
