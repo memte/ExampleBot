9930b65902017ddbc7e180d62f6b756c403f5ef2
Mehmet <63320170+memte@users.noreply.github.com>
2023-02-16 23:47:33 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,32 +9,44 @@ import { Routes } from 'discord-api-types/v10';
 let token = config.token;
 
 client.commands = new Collection()
+client.slashcommands = new Collection()
+client.commandaliases = new Collection()
 
+const rest = new REST({ version: '10' }).setToken(token);
 
-const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
+const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
 
 //command-handler
 const commands = []
-  readdirSync(`./src/commands`).forEach(async file => {
-    const command = await import(`./src/commands/${file}`).then(c => c.default)
-    commands.push(command.data.toJSON());
-    client.commands.set(command.data.name, command)
-  })
+readdirSync('./src/commands/normal').forEach(async file => {
+  const command = await import(`./src/commands/normal/${file}`).then(c => c.default)
+  if(command) {
+    client.commands.set(command.name, command)
+    commands.push(command.name, command);
+    if(command.aliases && Array.isArray(command.aliases)) {
+       command.aliases.forEach(alias => {
+        client.commandaliases.set(alias, command.name)  
+})
+}}})
 
-const rest = new REST({ version: '10' }).setToken(token);
+//slash-command-handler
+const slashcommands = [];
+readdirSync('./src/commands/slash').forEach(async file => {
+  const command = await import(`./src/commands/slash/${file}`).then(c => c.default)
+  slashcommands.push(command.data.toJSON());
+  client.slashcommands.set(command.data.name, command);
+})
 
 client.on("ready", async () => {
         try {
             await rest.put(
                 Routes.applicationCommands(client.user.id),
-                { body: commands },
+                { body: slashcommands },
             );
-
-		    log(`${client.commands.size} Komut yüklendi ve yenilendi!`);
         } catch (error) {
             console.error(error);
         }
-    log(`${client.user.username} Başarıyla Aktif Edildi!`);
+    log(`${client.user.username} Aktif Edildi!`);
 })
 
 //event-handler
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  prefix: "/",
+  prefix: "!",
   owner: "690639157013381220",
-  token: "Bot Tokeniniz"
+  token: "BOT Tokeni",
 }
