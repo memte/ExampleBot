ad4672b731836141b7c4ea9c354e2e96e9c18fc3
Mehmet <63320170+memte@users.noreply.github.com>
2023-04-06 19:35:42 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -7,7 +7,7 @@ export default {
    	 if (interaction.type == InteractionType.ApplicationCommand) {
    	 if(interaction.user.bot) return;
 	try {
-         const command = client.commands.get(interaction.commandName)
+         const command = client.slashcommands.get(interaction.commandName)
          command.run(client, interaction)
 	} catch {
 	interaction.reply({content: "Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.", ephemeral: true})
