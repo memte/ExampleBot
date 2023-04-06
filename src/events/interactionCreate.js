a907d7701096f6487aeed1287205f2c5357ab0eb
Naozumi <naozumi@naozumi.me>
2023-04-07 00:19:55 +0800

    Fix slashcommands using commands group

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -7,7 +7,7 @@ const { InteractionType } = require("discord.js");
    	 if (interaction.type == InteractionType.ApplicationCommand) {
    	 if(interaction.user.bot) return;
 	try {
-         const command = client.commands.get(interaction.commandName)
+         const command = client.slashcommands.get(interaction.commandName)
          command.run(client, interaction)
 	} catch {
 	interaction.reply({content: "Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.", ephemeral: true})
