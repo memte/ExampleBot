ca6498c9248da7995eaf80181abc78030455fc13
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-07-16 00:43:00 +0530

    Updated Discord bot code with command

--- index.js
+++ index.js
@@ -1,74 +1,110 @@
-const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  Partials,
+} = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.MessageContent,
+  ],
+  shards: "auto",
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Reaction,
+    Partials.GuildScheduledEvent,
+    Partials.User,
+    Partials.ThreadMember,
+  ],
+});
 const config = require("./src/config.js");
-const { readdirSync } = require("fs")
+const { readdirSync } = require("fs");
 const moment = require("moment");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
 
-let token = config.token
+let token = config.token;
 
-client.commands = new Collection()
-client.slashcommands = new Collection()
-client.commandaliases = new Collection()
+client.commands = new Collection();
+client.slashcommands = new Collection();
+client.commandaliases = new Collection();
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
-const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
+const log = (x) => {
+  console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`);
+};
 
-//command-handler
-const commands = []
-readdirSync('./src/commands/normal').forEach(async file => {
+// Command handler
+const commands = [];
+readdirSync("./src/commands/normal").forEach(async (file) => {
   const command = await require(`./src/commands/normal/${file}`);
-  if(command) {
-    client.commands.set(command.name, command)
+  if (command) {
+    client.commands.set(command.name, command);
     commands.push(command.name, command);
-    if(command.aliases && Array.isArray(command.aliases)) {
-       command.aliases.forEach(alias => {
-        client.commandaliases.set(alias, command.name)  
-})
-}}})
+    if (command.aliases && Array.isArray(command.aliases)) {
+      command.aliases.forEach((alias) => {
+        client.commandaliases.set(alias, command.name);
+      });
+    }
+  }
+});
 
-//slash-command-handler
+// Slash command handler
 const slashcommands = [];
-readdirSync('./src/commands/slash').forEach(async file => {
+readdirSync("./src/commands/slash").forEach(async (file) => {
   const command = await require(`./src/commands/slash/${file}`);
   slashcommands.push(command.data.toJSON());
   client.slashcommands.set(command.data.name, command);
-})
+});
 
 client.on(Events.ClientReady, async () => {
-        try {
-            await rest.put(
-                Routes.applicationCommands(client.user.id),
-                { body: slashcommands },
-            );
-        } catch (error) {
-            console.error(error);
-        }
-    log(`${client.user.username} Aktif Edildi!`);
-})
+  try {
+    await rest.put(Routes.applicationCommands(client.user.id), {
+      body: slashcommands,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  log(`${client.user.username} Aktif Edildi!`);
+});
 
-//event-handler
-readdirSync('./src/events').forEach(async file => {
-	const event = await require(`./src/events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-})
+// Event handler
+readdirSync("./src/events").forEach(async (file) => {
+  const event = await require(`./src/events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+});
 
-//process-listeners
-process.on("unhandledRejection", e => { 
-   console.log(e)
- }) 
-process.on("uncaughtException", e => { 
-   console.log(e)
- })  
-process.on("uncaughtExceptionMonitor", e => { 
-   console.log(e)
- })
-//
+// Process listeners
+process.on("unhandledRejection", (e) => {
+  console.log(e);
+});
+process.on("uncaughtException", (e) => {
+  console.log(e);
+});
+process.on("uncaughtExceptionMonitor", (e) => {
+  console.log(e);
+});
 
-client.login(token)
+client.login(token);
