1efcac5323e69b70d5e612bb6bde496fbadc842d
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-05 02:50:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
 const client = new Client({
   intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent ],
-  shards: "auto",
   partials: [ Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember ],
+  shards: "auto"
 });
 const config = require("./src/config.js");
 const { readdirSync } = require("fs");
