971bbf213e521784f497d670e7dad563cc85b4c6
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-05 02:50:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,15 +18,35 @@ client.commandaliases = new Collection()
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
+const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+  intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent ],
+  shards: "auto",
+  partials: [ Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember ],
+});
+const config = require("./src/config.js");
+const { readdirSync } = require("fs");
+const moment = require("moment");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+
+let token = config.token;
+
+client.commands = new Collection();
+client.slashcommands = new Collection();
+client.commandaliases = new Collection();
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+function log(message) {
+  console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${message}`);
+};
 
 //command-handler
-const commands = []
 readdirSync('./src/commands/normal').forEach(async file => {
   const command = await import(`./src/commands/normal/${file}`).then(c => c.default)
   if(command) {
     client.commands.set(command.name, command)
-    commands.push(command.name, command);
     if(command.aliases && Array.isArray(command.aliases)) {
        command.aliases.forEach(alias => {
         client.commandaliases.set(alias, command.name)  
