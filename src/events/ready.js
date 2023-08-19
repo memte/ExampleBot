186ad05ae22925cadcbf70db5a49463a94bddfd5
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-20 01:44:53 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,6 +5,10 @@ module.exports = {
  once: true,
  execute(client) {
   let activities = [ `Developed by memte.`, `${client.user.username}` ], i = 0;
-  setInterval(() => client.user.presence.set({ activities: [{ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }]}), 120000);
+  function botPresence() {
+  client.user.presence.set({ activities: [{ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }]})
+  setInterval(botPresence, 120000)
+  }
+  botPresence()
   client.log(`${client.user.username} Aktif Edildi!`);
 }};
