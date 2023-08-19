811d5ce110851bf1879f91fe7f6be5b28d98a3f2
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-20 01:24:10 +0300

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,8 +4,8 @@ const ms = require("ms")
 const cooldown = new Collection()
 
 module.exports = {
-	name: Events.MessageCreate,
-	execute: async(message) => {
+ name: Events.MessageCreate,
+ execute: async(message) => {
   let client = message.client;
   if (message.author.bot) return;
   if (message.channel.type === ChannelType.DM) return;
