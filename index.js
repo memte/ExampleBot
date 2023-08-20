7f4ba5f1ddcfa16050ef2893f7cef3e4271efb2f
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-20 16:26:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,36 +7,15 @@ const client = new Client({
 import config from "./src/config.js";
 import { readdirSync } from "node:fs";
 import moment from "moment";
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v10';
 
 let token = config.token;
 
 client.commands = new Collection()
-client.slashcommands = new Collection()
 client.commandaliases = new Collection()
-
-const rest = new REST({ version: '10' }).setToken(token);
-
-const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
-const client = new Client({
-  intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent ],
-  shards: "auto",
-  partials: [ Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember ],
-});
-const config = require("./src/config.js");
-const { readdirSync } = require("fs");
-const moment = require("moment");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v10");
+client.slashcommands = new Collection()
+client.slashDatas = new Collection()
 
 let token = config.token;
-
-client.commands = new Collection();
-client.slashcommands = new Collection();
-client.commandaliases = new Collection();
-
-const rest = new REST({ version: "10" }).setToken(token);
 
 function log(message) {
   console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${message}`);
@@ -58,23 +37,9 @@ readdirSync('./src/commands/prefix').forEach(async file => {
 const slashcommands = [];
 readdirSync('./src/commands/slash').forEach(async file => {
   const command = await import(`./src/commands/slash/${file}`).then(c => c.default)
-  slashcommands.push(command.data.toJSON());
+  client.slashDatas.push(command.data.toJSON());
   client.slashcommands.set(command.data.name, command);
 })
-
-async function whenReadyClient() {
-    if(!client.readyAt){
-        setTimeout(whenReadyClient, 1000)
-    } else {
-  try {
-    await rest.put(Routes.applicationCommands(client.user.id), {
-      body: slashcommands,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}}
-whenReadyClient()
 
 // Event Handler
 readdirSync('./src/events').forEach(async file => {
