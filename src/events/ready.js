dc57858691f0dcc3261fb6d55f4498e75e8b1f7c
Mehmet <63320170+memte@users.noreply.github.com>
2023-08-20 16:27:25 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,13 @@
 import { ActivityType, Events } from "discord.js";
 const config = require("../config.js")
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
 
 export default {
  name: Events.ClientReady,
  once: true,
  execute(client) {
+  const rest = new REST({ version: "10" }).setToken(config.token);
   let activities = [ `Developed by memte.`, `${client.user.username}` ], i = 0;
   function botPresence() {
   client.user.presence.set({ activities: [{ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }]})
@@ -12,4 +15,12 @@ export default {
   }
   botPresence()
   client.log(`${client.user.username} Aktif Edildi!`)
+  //
+  try {
+    await rest.put(Routes.applicationCommands(client.user.id), {
+      body: client.slashDatas,
+    });
+  } catch (error) {
+    console.error(error);
+  }
  }};
