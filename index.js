20ba6893ae4d5ad81caacbe1895d3c94050a5536
Mehmet <63320170+memte@users.noreply.github.com>
2023-11-15 17:58:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Client, Events, Partials, GatewayIntentBits, Collection } from "discord
 const client = new Client({
   intents: [GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
   partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember],
-  shards: "auto"s
+  shards: "auto"
 });
 import config from "./src/config.js";
 import { readdirSync } from "node:fs";
