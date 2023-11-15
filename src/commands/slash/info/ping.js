f284ff159d7d53f4194afcb4df54815d456f031c
Mehmet <63320170+memte@users.noreply.github.com>
2023-11-15 18:00:10 +0300

    Update ping.js

--- src/commands/slash/info/ping.js
+++ src/commands/slash/info/ping.js
@@ -4,7 +4,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Pong!"),s
+    .setDescription("Pong!"),
     // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
     cooldown: 5000,//1 saniye = 1000 ms / cooldown olmasını istemezseniz 0 yazın.
     ownerOnly: false,//komutu sadece geliştiricinin kullanabilmesini istersen true olarak değiştir
