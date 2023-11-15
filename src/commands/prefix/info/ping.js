bb17e110932d2f6126c33e402abf4784c95e6ed9
Mehmet <63320170+memte@users.noreply.github.com>
2023-11-15 17:59:44 +0300

    Update ping.js

--- src/commands/prefix/info/ping.js
+++ src/commands/prefix/info/ping.js
@@ -3,7 +3,7 @@ import { EmbedBuilder, PermissionsBitField } from "discord.js";
 export default {
     name: "ping",
     aliases: ["pong"],
-    cooldown: 5000,//1 saniye = 1000 ms / cooldown olmasını istemezseniz 0 yazın.
+    cooldown: 5000,//s1 saniye = 1000 ms / cooldown olmasını istemezseniz 0 yazın.
     ownerOnly: true,//komutu sadece geliştiricinin kullanabilmesini istersen true olarak değiştir
     run: async (client, message, args) => {
       message.reply(`Pong 🏓`)
