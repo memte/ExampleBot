9ee5e7611a77707572cf10333efea798c9ef9469
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 21:01:35 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,26 +1,28 @@
-import { ActivityType, Events } from "discord.js";
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v10';
+import {ActivityType, Events} from 'discord.js';
+import {REST} from '@discordjs/rest';
+import {Routes} from 'discord-api-types/v10';
 
 export default {
- name: Events.ClientReady,
- once: true,
- execute: async(client) => {
-  const rest = new REST({ version: "10" }).setToken(client.token);
-  const activities = [ `Developed by memte.`, `${client.user.username}` ]
-  let nowActivity = 0;
-  function botPresence() {
-  client.user.presence.set({ activities: [{ name: `${activities[nowActivity++ % activities.length]}`, type: ActivityType.Listening }]})
-  setTimeout(botPresence, 300000)
-  }
-  botPresence()
-  client.log(`${client.user.username} Aktif Edildi!`)
-  //
-  try {
-    await rest.put(Routes.applicationCommands(client.user.id), {
-      body: client.slashDatas,
-    });
-  } catch (error) {
-    console.error(error);
-  }
- }};
+	name: Events.ClientReady,
+	once: true,
+	async execute(client) {
+		const rest = new REST({version: '10'}).setToken(client.token);
+		const activities = ['Developed by memte.', `${client.user.username}`];
+		let nowActivity = 0;
+		function botPresence() {
+			client.user.presence.set({activities: [{name: `${activities[nowActivity++ % activities.length]}`, type: ActivityType.Listening}]});
+			setTimeout(botPresence, 300000);
+		}
+
+		botPresence();
+		client.log(`${client.user.username} Aktif Edildi!`);
+		//
+		try {
+			await rest.put(Routes.applicationCommands(client.user.id), {
+				body: client.slashDatas,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	},
+};
