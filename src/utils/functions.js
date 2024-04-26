c84f569ca695c3800daf48c7a0c70c3f2349a77c
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 20:59:58 +0300

    Update functions.js

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,9 @@
-import moment from "moment";
+import moment from 'moment';
 
 export default {
-    execute: async(client) => {
-        client.log = function (message) {
-            console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${message}`);
-          };
-    }
-}
+	async execute(client) {
+		client.log = function (message) {
+			console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] ${message}`);
+		};
+	},
+};
