a027a76a79597551f5132811d22c3cc368e5286a
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 21:01:53 +0300

    Update ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,21 +1,21 @@
-import { EmbedBuilder, PermissionsBitField } from "discord.js";
-import { SlashCommandBuilder } from "@discordjs/builders";
+import {EmbedBuilder, PermissionsBitField} from 'discord.js';
+import {SlashCommandBuilder} from '@discordjs/builders';
 
- export const commandBase = {
-  prefixData: {
-  name: "ping",
-  aliases: ["pong"]
-  },
-  slashData: new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Pong!"),
-  // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
-  cooldown: 5000,//1 saniye = 1000 ms / cooldown olmasını istemezseniz 0 yazın.
-  ownerOnly: false,//komutu sadece geliştiricinin kullanabilmesini istersen true olarak değiştir
-  prefixRun: async (client, message, args) => {
-    message.reply(`Pong 🏓`)
-  },
-  slashRun: async (client, interaction) => {
-    interaction.reply(`Pong 🏓`)
-  }
-}
+export const commandBase = {
+	prefixData: {
+		name: 'ping',
+		aliases: ['pong'],
+	},
+	slashData: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Pong!'),
+	// Komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
+	cooldown: 5000, // 1 saniye = 1000 ms / cooldown olmasını istemezseniz 0 yazın.
+	ownerOnly: false, // Komutu sadece geliştiricinin kullanabilmesini istersen true olarak değiştir
+	async prefixRun(client, message, args) {
+		message.reply('Pong 🏓');
+	},
+	async slashRun(client, interaction) {
+		interaction.reply('Pong 🏓');
+	},
+};
