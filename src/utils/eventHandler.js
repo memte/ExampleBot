87e0d92a20506320ee341871191a54a0fdf3b5ae
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 20:59:41 +0300

    Update eventHandler.js

--- src/utils/eventHandler.js
+++ src/utils/eventHandler.js
@@ -1,28 +1,28 @@
-import { readdirSync } from "node:fs";
+import {readdirSync} from 'node:fs';
 
 export default {
-    execute: async(client) => {
-        const eventFiles = readdirSync("./src/events");
+	async execute(client) {
+		const eventFiles = readdirSync('./src/events');
 
-        Promise.all(eventFiles.map(async (file) => {
-        const event = await import(`../events/${file}`).then(x => x.default);
-        
-        if (event.once) {
-          client.once(event.name, (...args) => event.execute(...args));
-        } else {
-          client.on(event.name, (...args) => event.execute(...args));
-        }
-        }));
-        
-        // Process Listeners
-        process.on("unhandledRejection", e => { 
-           console.log(e)
-         }) 
-        process.on("uncaughtException", e => { 
-           console.log(e)
-         })  
-        process.on("uncaughtExceptionMonitor", e => { 
-           console.log(e)
-         })
-    }
-}
+		Promise.all(eventFiles.map(async file => {
+			const event = await import(`../events/${file}`).then(x => x.default);
+
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args));
+			} else {
+				client.on(event.name, (...args) => event.execute(...args));
+			}
+		}));
+
+		// Process Listeners
+		process.on('unhandledRejection', e => {
+			console.log(e);
+		});
+		process.on('uncaughtException', e => {
+			console.log(e);
+		});
+		process.on('uncaughtExceptionMonitor', e => {
+			console.log(e);
+		});
+	},
+};
