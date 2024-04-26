64aa80a15782a650358146e42dd26c371ac15b86
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 20:21:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const {Client, GatewayIntentBits, Partials} = require('discord.js');
 const client = new Client({
-  intents: Object.values(GatewayIntentBits), 
-  partials: Object.values(Partials),
-  shards: "auto"
+	intents: Object.values(GatewayIntentBits),
+	partials: Object.values(Partials),
+	shards: 'auto',
 });
-const config = require("./src/config.js");
-const { readdirSync } = require("node:fs");
+const config = require('./src/config.js');
+const {readdirSync} = require('node:fs');
 
-let token = config.token;
+const {token} = config;
 
-readdirSync("./src/utils").map(async (file) => {
-const util = await require(`./src/utils/${file}`);
-util.execute(client);
+readdirSync('./src/utils').map(async file => {
+	const util = await require(`./src/utils/${file}`);
+	util.execute(client);
 });
 
 client.login(token);
