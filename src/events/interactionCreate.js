64b0b21b30da5fc3d9f5ae9afe3c0aa13d477f7d
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 20:22:55 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,41 +1,47 @@
-const { Collection, Events, InteractionType } = require("discord.js");
+const {Collection, Events, InteractionType} = require('discord.js');
 const cooldown = new Collection();
-const config = require("../config.js");
+const config = require('../config.js');
 
 module.exports = {
- name: Events.InteractionCreate,
+	name: Events.InteractionCreate,
 
- execute: async(interaction) => {
-    let client = interaction.client;
-    if (interaction.type == InteractionType.ApplicationCommand) {
-      if (interaction.user.bot) return;
-    
-      try {
-        const command = client.slashCommands.get(interaction.commandName)
-        if(command.ownerOnly && interaction.user.id !== config.owner) return interaction.reply({content: "Bu komutu sadece **geliştiricim** kullanabilir.", ephemeral: true});
-        if (command.cooldown) {
-        if (cooldown.has(`${command.name}-${interaction.user.id}`)) {
-        const nowDate = interaction.createdTimestamp;
-        const waitedDate = cooldown.get(`${command.name}-${interaction.user.id}`) - nowDate;
-          return interaction.reply({
-            content: `Cooldown şu an aktif, lütfen <t:${Math.floor(new Date(nowDate + waitedDate).getTime() / 1000)}:R> tekrar deneyin.`,
-            ephemeral: true
-          }).then(() => setTimeout(() => interaction.deleteReply(), cooldown.get(`${command.name}-${interaction.user.id}`) - Date.now() + 1000));
-        }
-          command.slashRun(client, interaction);
+	async execute(interaction) {
+		const {client} = interaction;
+		if (interaction.type === InteractionType.ApplicationCommand) {
+			if (interaction.user.bot) {
+				return;
+			}
 
-          cooldown.set(`${command.name}-${interaction.user.id}`, Date.now() + command.cooldown);
+			try {
+				const command = client.slashCommands.get(interaction.commandName);
+				if (command.ownerOnly && interaction.user.id !== config.owner) {
+					return interaction.reply({content: 'Bu komutu sadece **geliştiricim** kullanabilir.', ephemeral: true});
+				}
 
-          setTimeout(() => {
-            cooldown.delete(`${command.name}-${interaction.user.id}`);
-          }, command.cooldown + 1000);
-        } else {
-        command.slashRun(client, interaction)
-      }
-      } catch (e) {
-        console.error(e)
-        interaction.reply({content: "Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.", ephemeral: true})
-      }
-    }
-  }
-}
+				if (command.cooldown) {
+					if (cooldown.has(`${command.name}-${interaction.user.id}`)) {
+						const nowDate = interaction.createdTimestamp;
+						const waitedDate = cooldown.get(`${command.name}-${interaction.user.id}`) - nowDate;
+						return interaction.reply({
+							content: `Cooldown şu an aktif, lütfen <t:${Math.floor(new Date(nowDate + waitedDate).getTime() / 1000)}:R> tekrar deneyin.`,
+							ephemeral: true,
+						}).then(() => setTimeout(() => interaction.deleteReply(), cooldown.get(`${command.name}-${interaction.user.id}`) - Date.now() + 1000));
+					}
+
+					command.slashRun(client, interaction);
+
+					cooldown.set(`${command.name}-${interaction.user.id}`, Date.now() + command.cooldown);
+
+					setTimeout(() => {
+						cooldown.delete(`${command.name}-${interaction.user.id}`);
+					}, command.cooldown + 1000);
+				} else {
+					command.slashRun(client, interaction);
+				}
+			} catch (e) {
+				console.error(e);
+				interaction.reply({content: 'Komut çalıştırılırken bir sorunla karşılaşıldı! Lütfen tekrar deneyin.', ephemeral: true});
+			}
+		}
+	},
+};
