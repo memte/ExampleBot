778e8942ec631d9da6a279542d3c38dfc8ce49a8
Mehmet <63320170+memte@users.noreply.github.com>
2024-04-26 20:21:54 +0300

    Update commandHandler.js

--- src/utils/commandHandler.js
+++ src/utils/commandHandler.js
@@ -1,45 +1,45 @@
-const { Collection } = require("discord.js");
-const { readdirSync } = require("node:fs");
+const {Collection} = require('discord.js');
+const {readdirSync} = require('node:fs');
 
 module.exports = {
-    execute: async (client) => {
-      client.commands = new Collection();
-      client.commandAliases = new Collection();
-      client.slashCommands = new Collection();
-      client.slashDatas = [];
-  
-      // - Handlers -
-      const commandFolders = await readdirSync("./src/commands");
-  
-      await Promise.all(commandFolders.map(async (category) => {
-        const commandFiles = await readdirSync(`./src/commands/${category}`);
-  
-        await Promise.all(commandFiles.map(async (file) => {
-          const commands = await import(`../commands/${category}/${file}`);
-  
-          if (commands) {
-            if (commands.commandBase) {
-              if (commands.commandBase.prefixData) {
-                // Prefix Command
-                const prefixCommand = commands.commandBase;
-                client.commands.set(prefixCommand.prefixData.name, prefixCommand);
-  
-                if (prefixCommand.prefixData.aliases && Array.isArray(prefixCommand.prefixData.aliases)) {
-                  prefixCommand.prefixData.aliases.forEach(alias => {
-                    client.commandAliases.set(alias, prefixCommand.prefixData.name);
-                  });
-                }
-              }
-            }
-  
-            if (commands.commandBase && commands.commandBase.slashData) {
-              // Slash Command
-              const slashCommand = commands.commandBase;
-              client.slashDatas.push(slashCommand.slashData.toJSON());
-              client.slashCommands.set(slashCommand.slashData.name, slashCommand);
-            }
-          }
-        }));
-      }));
-    },
-}
+	async execute(client) {
+		client.commands = new Collection();
+		client.commandAliases = new Collection();
+		client.slashCommands = new Collection();
+		client.slashDatas = [];
+
+		// - Handlers -
+		const commandFolders = await readdirSync('./src/commands');
+
+		await Promise.all(commandFolders.map(async category => {
+			const commandFiles = await readdirSync(`./src/commands/${category}`);
+
+			await Promise.all(commandFiles.map(async file => {
+				const commands = await import(`../commands/${category}/${file}`);
+
+				if (commands) {
+					if (commands.commandBase) {
+						if (commands.commandBase.prefixData) {
+							// Prefix Command
+							const prefixCommand = commands.commandBase;
+							client.commands.set(prefixCommand.prefixData.name, prefixCommand);
+
+							if (prefixCommand.prefixData.aliases && Array.isArray(prefixCommand.prefixData.aliases)) {
+								prefixCommand.prefixData.aliases.forEach(alias => {
+									client.commandAliases.set(alias, prefixCommand.prefixData.name);
+								});
+							}
+						}
+					}
+
+					if (commands.commandBase && commands.commandBase.slashData) {
+						// Slash Command
+						const slashCommand = commands.commandBase;
+						client.slashDatas.push(slashCommand.slashData.toJSON());
+						client.slashCommands.set(slashCommand.slashData.name, slashCommand);
+					}
+				}
+			}));
+		}));
+	},
+};
