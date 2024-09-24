4200f1d6af9b7ecee36a106a6c080397a5de0106
Mehmet <63320170+memte@users.noreply.github.com>
2024-09-24 22:58:35 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,7 +11,7 @@ module.exports = {
 			activities: [{name: 'Developed by memte.', type: 'LISTENING'}],
 		});
 
-		client.log(`${client.user.username} Aktif Edildi!`);
+		client.log(`${client.user.username} Active!`);
 		//
 		try {
 			await rest.put(
