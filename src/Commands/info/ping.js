79fbc1d0d47265c301401bfb36f5c621238bd789
Mehmet <63320170+memte@users.noreply.github.com>
2024-10-14 18:55:58 +0300

    Update ping.js

--- src/Commands/info/ping.js
+++ src/Commands/info/ping.js
@@ -11,7 +11,7 @@ export const commandBase = {
 		.setDescription('Pong!'),
 	// If you want to improve the command, check the guide: https://v13.discordjs.guide/interactions/slash-commands.html#options
 	cooldown: 5000, // 1 second = 1000 ms / set to 0 if you don't want a cooldown.
-	ownerOnly: true, // Set to true if you want the command to be usable only by the developer.
+	ownerOnly: false, // Set to true if you want the command to be usable only by the developer.
 	async prefixRun(client, message, args) {
 		message.reply('Pong 🏓');
 	},
